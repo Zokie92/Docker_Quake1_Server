9b474332c9fb3f4e4a204e7056db417345825ab3
Zokie92 <ducky222999@gmail.com>
2025-11-22 15:34:17 +0100

    delete files

--- Quake1_Docker_Server/game/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -f /game/id1/pak0.pak ]; then
-  echo "ERROR: /game/id1/pak0.pak saknas!"
-  exec sleep infinity
-fi
-
-exec /usr/local/bin/quakespasm -dedicated +port 26000 +map start +set sv_maxclients 8
